3a73950c468d9b5ab883b65a0595965a478bf6fb
campuzan21 <campuzan21@up.edu>
2018-11-29 18:18:19 -0800

    Created username function for campuzan21's favorite food

---
+++ campuzan21.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "campuzan21.h"
+
+void campuzan21() {
+	printf("campuzan21 likes heavy metals without nickel or copper. \n");
+
+}
---
+++ campuzan21.h
@@ -0,0 +1,2 @@
+extern void campuzan21();
+
